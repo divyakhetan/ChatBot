8c7b6afaa586b2ec3d386cfb9f0a9e3fd56bd822
divyakhetan <divyakhetan1209@gmail.com>
2019-03-23 16:17:04 +0530

    chatbot

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request, make_response
+import json
+import requests
+URL = "http://numbersapi.com/"
+
+app = Flask(__name__)
+@app.route('/',  methods = ['GET'])
+
+
+def home():
+    return "hello from divya"
+
+@app.route('/', methods = ['POST'])
+def post():
+    #req =request.data
+    req = request.get_json(silent = True, force = False) #json to python dictionary
+    print(req)
+    intentName = req.get('queryResult').get('intent').get('displayName')
+    if(intentName == 'Default Welcome Intent'):
+        return jsonify({"fulfillmentText" : "Welcome!! You can now know cool facts about any number!! Enjoy! :)"})
+    elif(intentName == 'numbers'):
+        typeValue = req.get('queryResult').get("parameters").get("type")
+        number = int(req.get('queryResult').get("parameters").get("number"))
+        url =  URL + str(number) + '/' + typeValue + '?json'
+        response = requests.get(url)
+        txt = json.loads(response.text)["text"]
+        print(url)
+        return jsonify({"fulfillmentText" : txt})
+    #print(intentName)
+    
+    return jsonify(req)
+
+if(__name__ == "__main__"):
+    app.run()
